2ab358350dd646f291644bce3d89c813070153ec
Andi Darmawan <92540957+andidar83@users.noreply.github.com>
2024-03-07 15:23:37 +0700

    Update jira_api.py

--- jira_api.py
+++ jira_api.py
@@ -89,3 +89,114 @@ else:
 # # Print all issues
 # for issue in all_issues:
 #     print(issue)
+
+
+
+
+
+
+
+
+#################################################################################################################################################
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 15:58:37 2024
+
+@author: NXP
+"""
+
+import requests
+import pandas as pd
+# # Jira credentials
+# username = 'andi.darmawan@ninjavan.co'
+# api_token = 'NjIzOTQ5MzIzMDIxOop1cwJ5KiXZAfWOPRJYv4J0hz0o'
+# base_url = 'https://servicedesk.ninjavan.co'
+
+# # Authentication
+# auth = (username, api_token)
+
+# # Endpoint for getting issues
+# endpoint = f'{base_url}/rest/servicedeskapi/request'
+
+# response = requests.get(endpoint, auth=auth)
+
+# print(response)
+# print(response.text)
+
+
+# Jira credentials
+bearer_token = 'NjIzOTQ5MzIzMDIxOop1cwJ5KiXZAfWOPRJYv4J0hz0o'
+base_url = 'https://servicedesk.ninjavan.co'
+
+# Authentication headers
+headers = {
+    'Authorization': f'Bearer {bearer_token}',
+    'Accept': 'application/json'
+}
+
+jql_query = 'project = "ID Business Intelligence"'
+max_results = 1  # Replace 100 with the desired maximum number of results
+
+# Construct the URL with both JQL query and maxResults
+endpoint = f'{base_url}/rest/api/2/search?jql={jql_query}&maxResults={max_results}'
+
+
+
+# Make GET request
+response = requests.get(endpoint, headers=headers)
+print(response)
+# Check response status
+if response.status_code == 200:
+    # Extract issues from response
+    print(response.json())
+
+
+else:
+    print('Failed to retrieve issues:', response.text)
+# Extract 'issues' from response
+issues_data = response.json()['issues']
+
+# Convert to DataFrame
+df = pd.json_normalize(issues_data)
+df.to_excel('test.xlsx')
+print(df)
+# # Parameters for pagination
+# start_at = 0
+# max_results = 50  # Adjust as needed
+
+# # List to store all issues
+# all_issues = []
+
+# while True:
+#     # Parameters for pagination
+#     params = {
+#         'startAt': start_at,
+#         'maxResults': max_results
+#     }
+
+#     # Make GET request
+#     response = requests.get(endpoint, auth=auth, params=params)
+
+#     # Check if request was successful
+#     if response.status_code == 200:
+#         # Extract issues from response
+#         issues = response.json()['issues']
+        
+#         # Add issues to the list
+#         all_issues.extend(issues)
+        
+#         # Increment startAt for pagination
+#         start_at += max_results
+        
+#         # Break the loop if all issues have been retrieved
+#         if len(issues) < max_results:
+#             break
+#     else:
+#         print('Failed to retrieve issues:', response.text)
+#         break
+
+# # Print all issues
+# for issue in all_issues:
+#     print(issue)
+
